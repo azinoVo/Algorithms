673196e470ef0e0b830b6e1dbf481d103ddff606
azinoVo <azinovo22@gmail.com>
2019-09-12 15:29:09 -0500

    stock prices implem

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -40,8 +40,8 @@ def recipe_batches(recipe, ingredients):
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  # recipe = { 'milk': 2, 'sugar': 40, 'butter': 20 }
-  # ingredients = { 'milk': 5, 'sugar': 120, 'butter': 500 }
+  # recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+  # ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  recipe = { 'milk': 2, 'sugar': 40, 'butter': 20 }
+  ingredients = { 'milk': 5, 'sugar': 120, 'butter': 500 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
